ea2919a346b43660f64f4e8e6321af83de34f1f5
JacobLayton <jlayjlay3@gmail.com>
2019-05-15 16:29:11 -0700

    Tests added to spec and add function added to calculator

--- calculator/calculator.spec.js
+++ calculator/calculator.spec.js
@@ -1,3 +1,18 @@
-const { add } = require('./calculator.js');
-
+const { add } = require("./calculator.js");
+// SUT: System Under Test
+// suite > test cases > assertions > matchers
 // test away!
+test("adds two numbers", () => {
+  // Arrange (setup)
+  const expected = 4;
+
+  // Act (Run SUT)
+  const actual = add(2, 2);
+
+  // Assert
+  expect(actual).toBe(expected);
+
+  // Using the .toBe() matcher
+  expect(add(2, 2)).toBe(4);
+  expect(add(3, 1)).toBe(4);
+});
